621b5509f04bc6a9b954df31277ee8365b2cf390
Haris Ahmad Khan <hk08694@st.habib.edu.pk>
2026-04-13 19:10:05 +0500

    Develop two functions to fetch weather data and air pollution data respectively

--- Data_Fetch.py
+++ Data_Fetch.py
@@ -3,6 +3,44 @@ import json
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+API_KEY = None
 
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
+
+def fetch_current_weather(lat, lon):
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'lat': lat,
+        'lon': lon,
+        'appid': API_KEY,
+        'units': 'metric'
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error fetching data: {response.status_code}, {response.text}")
+        return None
+    
+def fetch_current_air_pollution(lat, lon):
+    url = "https://api.openweathermap.org/data/2.5/air_pollution"
+    params = {
+        'lat': lat,
+        'lon': lon,
+        'appid': API_KEY
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error fetching data: {response.status_code}, {response.text}")
+        return None
+
+
+def main():
+    global API_KEY
+    load_dotenv()
+    API_KEY = os.getenv('API_KEY')
